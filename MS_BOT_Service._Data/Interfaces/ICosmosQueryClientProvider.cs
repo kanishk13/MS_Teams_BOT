c5d5d208f9394ae18ae23e96f59c3652b8d09de4
kanishk13 <42895589+kanishk13@users.noreply.github.com>
2020-09-14 23:35:25 +0530

    Add files via upload

---
+++ MS_BOT_Service._Data/Interfaces/ICosmosQueryClientProvider.cs
@@ -0,0 +1,11 @@
+﻿namespace MS.GTA.BOTService.Data.Interfaces
+{
+    public interface ICosmosQueryClientProvider
+    {
+        /// <summary>Get or generate a Cosmos Query client.</summary>
+        /// <param name="containerName">Name of the container in database</param>
+        /// <param name="databaseName">Name of the database in CosmosDB account</param>
+        /// <returns>Query for the container</returns>
+        ICosmosQueryClient GetCosmosQueryClient(string databaseName, string containerName);
+    }
+}
